d898b0e7b25405e2b51d197adf4cf5d3739d5762
ZachLofquist <zlofquist@gmail.com>
2022-01-18 00:47:02 -0800

    Getting Users by Name

--- backend.js
+++ backend.js
@@ -28,7 +28,7 @@ const users = {
         },
         {
             id : "ppp222",
-            name : "Frank",
+            name : "Mac",
             job: "Owner",
         },
         {
@@ -44,9 +44,22 @@ const users = {
     ]
 }
 
+
 app.get('/users', (req, res) => {
-    res.send(users);
+    const name = req.query.name;
+    if (name != undefined){
+        let result = findUserByName(name);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else{
+        res.send(users);
+    }
 });
+
+const findUserByName = (name) => {
+    return users['users_list'].filter( (user) => user['name'] === name);
+}
 
 
 
